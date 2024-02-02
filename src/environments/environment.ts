5d3f8818baab364a923aecb6c7adf85b6b616845
Vignesh-R-3kt <vignesh.r@3ktechnologies.com>
2024-02-02 18:14:00 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
         authority: 'https://login.microsoftonline.com/818467cf-44b9-45a5-a41a-1ef924fcd795',
         resourceMapURL: 'https://graph.microsoft.com/v1.0/me',
         // postLogoutRedirectUri: 'http://localhost:4200'
-        postLogoutRedirectUri: 'https://3kt-helpdesk.netlify.app/'
+        postLogoutRedirectUri: 'https://helpdesk-server-dev.vercel.app/'
     },
 }
